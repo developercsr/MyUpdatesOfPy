5d0d7d947f6b9220d270943dd63685c761b3723d
developercsr <sumanthroyal111@gmail.com>
2024-01-01 12:17:10 +0530

    i pushed code  succesfullly

---
+++ Python_practice/python2.py
@@ -0,0 +1,8 @@
+#priority order in mathematical expressions
+#() > ** > -(negetion)>/(Divison)>//(Integer Divison)>*(Multiplication)>%>(modulus Divison)
+print(2**4)
+print(-4)
+print(45/4)
+print(55//4)
+print(41%4)
+print(34+(5**2*4)/(45%4/2//0.5))
