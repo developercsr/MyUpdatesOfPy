0a4e78baf8e69b5863f60a06c2a18eaf5a991e44
developercsr <sumanthroyal111@gmail.com>
2024-01-01 12:30:41 +0530

    i changes python first to learn how i ca print thr tsrings in python

--- Python_practice/python_start.py
+++ Python_practice/python_start.py
@@ -1,1 +1,12 @@
-print("Hello worlds")
+#Python ways to print the words
+print("Helllo world")
+print('I m sumanth')
+x="i completed my graduation"
+print(x)
+#Multi line strings
+y="""hii all
+i m sumanth
+i completed my graduation in 2023"""
+print(y)
+print(5+6)
+      
