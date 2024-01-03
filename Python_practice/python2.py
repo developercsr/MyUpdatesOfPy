6180a65e9e9969460e2780e733ae82df940ca6e4
sumanth <sumanthroyal111@gmail.com>
2024-01-03 17:21:15 +0530

    strings printing techniques are done

--- Python_practice/python2.py
+++ Python_practice/python2.py
@@ -6,3 +6,10 @@ print(45/4)
 print(55//4)
 print(41%4)
 print(34+(5**2*4)/(45%4/2//0.5))
+#Something if should repeat thrn multiply on riht side
+print("Hello\n"*5)
+print("hii"+"sumanth")#Concatinating
+print("""hii
+      This is a multiline 
+      string""")#multi line string
+print('We can use single quotations also')#We can use single quotations also
